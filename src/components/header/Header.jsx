d9e1ddd766ea1d0830118187fb55cc64f70e7380
Viraj <virajmalaka94@gmail.com>
2022-09-21 14:12:03 +0530

    Added header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import "./header.css";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
-const Header = () => {
+
+const Header = ({ type }) => {
+
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <div
+        className={
+          type === "list" ? "headerContainer listMode" : "headerContainer"
+        }
+      >
+        <div className="headerList">
+          <div className="headerListItem">
+            <span>Home</span>
+          </div>
+          <div className="headerListItem">
+            <span>Galary</span>
+          </div>
+          <div className="headerListItem">
+            <span>Contact us</span>
+          </div>
+          <div className="headerListItem">
+            <span>Help</span>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
