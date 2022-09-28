c3c59e15ce5993e5ec9cf6992b58ecca2cf8f033
Nimesha <lnimesha759@gmail.com>
2022-09-28 14:59:02 -0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Event from "./pages/event/Event";
 import Event_upcoming from "./pages/event_upcoming/event_upcoming";
 import Event_past from "./pages/event_past/event_past";
 import RegistrationForm from "./pages/registration/RegistrationForm";
+import Event_ID from "./pages/event_ID/event_ID";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path="/event_upcoming" element={<Event_upcoming/>}/>
         <Route path="/event_past" element={<Event_past/>}/>
         <Route path="/registration" element={<RegistrationForm/>}/>
+        <Route path="/event_ID" element={<Event_ID/>}/>
       </Routes>
     </BrowserRouter>
   );
