2896984d0c4a1e61ec26a43a117e61b56b933d56
Viraj <virajmalaka94@gmail.com>
2022-10-12 12:25:01 +0530

    modified list

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import "./list.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header"
+import SearchItem from "../../components/SearchItems/SearchItem";
+import DateSelector from "../../components/DateSelector/DateSelector";
+import {useState} from 'react';
 
 const List = () => {
+  const [openDate, setOpenDate] = useState(false)
   return (
-    <div>List</div>
+    <div>
+      <Navbar/>
+      <Header/>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h2 className="lsTitle">Search</h2>
+            <div className="lsItem">
+              <label>Location</label>
+              <input type="text" />
+            </div>
+            <div className="lsItem">
+              <span onClick={()=>setOpenDate(!openDate)}>Search date</span>
+            </div>
+            {openDate && <DateSelector/>}
+            
+
+          </div>
+          <div className="listResult">
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
