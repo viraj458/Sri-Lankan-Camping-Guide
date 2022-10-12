5b15b86287266a93c73a13db05bea7408a26b203
Viraj <virajmalaka94@gmail.com>
2022-10-12 12:29:03 +0530

    added searchItem

---
+++ src/components/SearchItems/SearchItem.jsx
@@ -0,0 +1,22 @@
+import "./searchItem.css";
+import site3 from "./site3.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar} from "@fortawesome/free-solid-svg-icons";
+const SearchItem = () => {
+  return (
+    <div className="searchItem">
+      <img src={site3} alt="" className="sItemImage" />
+      <div className="sItemDiscription">
+        <h3  className="sItemTitle">Eco Lodge Haputhale Camping </h3 >
+        <span className="sItemDetails">Eco Lodge Haputhale Camping is set in Haputhale.A full English/Irish breakfast is avalable every morning at campsite.</span>
+        <button className="sItemButton"><b>View</b></button>
+      </div>
+      <div className="sItemOther">
+        <FontAwesomeIcon icon={faStar} />
+        <div className="sItemRating">9.5</div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchItem
