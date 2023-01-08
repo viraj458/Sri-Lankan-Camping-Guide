59dcf517c3b9b071c4e2725e18a654778febc67b
Viraj <virajmalaka94@gmail.com>
2023-01-08 21:53:04 +0530

    update features

--- src/pages/listAll/listAll.jsx
+++ src/pages/listAll/listAll.jsx
@@ -1,22 +1,22 @@
-import "./list.css";
+import "./listAll.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header"
 import SearchItem from "../../components/SearchItems/SearchItem";
 import {useState} from 'react';
-import { useLocation, useNavigate} from "react-router-dom";
+// import { useLocation, useNavigate} from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 
-const List = () => {
+const ListAll = () => {
 
  
-  const location = useLocation();
-  const [destination, setDestination] = useState(location.state)
+//   const location = useLocation();
+//   const [destination, setDestination] = useState(location.state)
 
   const handleClick = () => {
     reFetch()
   }
 
-  const {data, loading, error, reFetch} = useFetch(`http://localhost:5000/api/v1/campsites?nearest_city=${destination}`)
+  const {data, loading, error, reFetch} = useFetch("http://localhost:5000/api/v1/allcampsites")
 
   return (
     <div>
@@ -24,7 +24,7 @@ const List = () => {
       <Header />
       <div className="listContainer">
         <div className="listWrapper">
-          <div className="listSearch">
+          {/* <div className="listSearch">
             <h2 className="lsTitle">Search</h2>
             <div className="lsItem">
               <label>Location</label>
@@ -39,7 +39,7 @@ const List = () => {
             </div>
             
 
-          </div>
+          </div> */}
           <div className="listResult">
             {loading ? "Loading" : <>
             {data.map(item=>(
@@ -54,4 +54,4 @@ const List = () => {
   )
 }
 
-export default List
+export default ListAll
