33a39b8046340280ee79a4712651d96b6baf3a1c
Viraj <virajmalaka94@gmail.com>
2023-01-08 17:00:30 +0530

    update SearchItem page

--- src/components/SearchItems/SearchItem.jsx
+++ src/components/SearchItems/SearchItem.jsx
@@ -6,12 +6,12 @@ import {Link } from "react-router-dom";
 
 const SearchItem = ({item}) => {
   return (
-    <div className="searchItem">
+    <div className="searchItem" key={item._id}>
       <img src={item.photos_of_location[0]} alt="" className="sItemImage" />
       <div className="sItemDiscription">
         <h3  className="sItemTitle">{item.campsite_name}</h3 >
         <span className="sItemDetails">{item.description}</span>
-        <Link to={`http://localhost:5000/api/v1/campsites/${item._id}`}><button className="sItemButton"><b>View</b></button></Link>
+        <Link to={`/campsite/${item._id}`}><button className="sItemButton"><b>View</b></button></Link>
       </div>
       {/* <div className="sItemOther">
         <FontAwesomeIcon icon={faStar} />
--- src/pages/campsite/Campsite.jsx
+++ src/pages/campsite/Campsite.jsx
@@ -1,4 +1,6 @@
 import './campsite.css'
+import { useLocation } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header'
@@ -9,6 +11,10 @@ import Footer from '../../components/footer/Footer';
 
 
 const Campsite = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const { data, loading, error } = useFetch(`http://localhost:5000/api/v1/campsite/find/${id}`);
+
 
   const photos = [
     {src:'https://media.istockphoto.com/id/911995140/photo/camping-tent-in-a-camping-in-a-forest-by-the-river.jpg?s=612x612&w=0&k=20&c=VTK_4PQ1fIRol9S40-3uXjJEve9HwbKOeginDDbpYZk='},
@@ -22,29 +28,32 @@ const Campsite = () => {
     <div>
       <Navbar type='list'/>
       <Header/>
-      <div className="campsiteContainer">
+      {loading ? ("Loading") : (
+        <>
+        <div className="campsiteContainer">
         <div className="campsiteWrapper">
-          <h1 className="campsiteTitle">Asiri Campsite</h1>
+          <h1 className="campsiteTitle">{data.campsite_name}</h1>
           <div className="campsiteAddress">
             <FontAwesomeIcon icon={faLocationDot}/>
-            <span>Imaduwa, Galle</span>
+            <span>{data.location_address}</span>
           </div>
+         
           <div className="campsiteImages">
-            {photos.map(photo=>(
-              <div className="campsiteImageWrapper">
-                <img src={photo.src} alt="" className="campsiteImage" />
-              </div>
-            ))}
-          </div>
+              {data.photos_of_location?.map((photo) => (
+                <div className="campsiteImageWrapper">
+                  <img
+                    src={photo}
+                    alt=""
+                    className="campsiteImage"
+                  />
+                </div>
+              ))}
+            </div>
           <hr />
           <div className="campsiteDetails">
             <h1>Info</h1>
             
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.
-            </p>
+            <p>{data.description}</p>
           </div>
           <hr />
           <div className="packageDetails">
@@ -52,19 +61,13 @@ const Campsite = () => {
             <Packages/>
           </div>
           <hr />
-          <div className="campsiteRules">
-              <h1>Rules</h1>
-              <div className="rulesInfo">
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.</li>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.</li>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.</li>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt perspiciatis cupiditate placeat magnam accusamus recusandae, provident esse alias tenetur aut distinctio, incidunt in temporibus nihil reiciendis explicabo, dicta dolor unde.</li>
-              
-              </div>
-          </div>
+  
         </div>
         <Footer/>
       </div>
+        </>
+      ) }
+      
       
     </div>
   )
